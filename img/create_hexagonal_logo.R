88afd5223ffcb2aadfc760890d32bf3c02d92ec9
Sadraque <sadraquelucena@academico.ufs.br>
2025-04-25 18:13:59 -0300

    logo final

--- img/create_hexagonal_logo.R
+++ img/create_hexagonal_logo.R
@@ -1,7 +1,9 @@
+# logo criada com https://www.design.com/
+
 library(hexSticker)
 
 sticker(
-  subplot = "/home/sadraque/Downloads/websitetest/img/logo7.png",   # sua imagem retangular
+  subplot = "/home/sadraque/Downloads/websitetest/img/logo.png",   # sua imagem retangular
   s_x = 1, s_y = 1, s_width = 1.2,  # ajustes da imagem
   package = "",        # nome do pacote/curso
   p_size = 16,                  # tamanho do texto
@@ -10,5 +12,5 @@ sticker(
   h_color = "#2A6420",          # cor da borda
   white_around_sticker = TRUE,   # remove tudo fora do hexágono
   asp = 1,                       # mantém proporção
-  filename = "img/logo_hex8.png"     # nome do arquivo final
+  filename = "img/ESTAT0090_logo_hex.png"     # nome do arquivo final
 )
