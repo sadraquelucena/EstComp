00747c6831c7cdda068a9c96cbfc5581cb1b664e
Sadraque <sadraquelucena@academico.ufs.br>
2025-04-26 00:11:44 -0300

    logo com fundo transparente

--- img/create_hexagonal_logo.R
+++ img/create_hexagonal_logo.R
@@ -8,9 +8,19 @@ sticker(
   package = "",        # nome do pacote/curso
   p_size = 16,                  # tamanho do texto
   p_color = "white",           # cor do texto
-  h_fill = "transparent",           # cor do fundo
+  h_fill = "#BED5BE",           # cor do fundo
   h_color = "#2A6420",          # cor da borda
   white_around_sticker = TRUE,   # remove tudo fora do hexágono
   asp = 1,                       # mantém proporção
   filename = "img/ESTAT0090_logo_hex.png"     # nome do arquivo final
 )
+
+library(magick)
+p <- image_read("img/ESTAT0090_logo_hex.png")
+pp <- p %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = 4, point = "+1+1") %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = 4, point = "+517+1") %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = 4, point = "+1+599") %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = 4, point = "+517+599")
+image_write(image = pp, path = "img/ESTAT0090_logo_hex.png")
+file.show("img/ESTAT0090_logo_hex.png")
